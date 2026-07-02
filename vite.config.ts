0c73a28bfca10904c2aafc23b59716f34c102405
Luquitaabz <lucassoto863@gmail.com>
2026-07-02 11:56:19 -0400

    Fix merge conflicts in vite.config.ts
    Resolve merge conflicts in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> d2f90cedf1ae15b6a4be2def582eb7f514bb1e43
   server: {
     proxy: {
       '/api': {
@@ -19,11 +15,6 @@ export default defineConfig({
       },
     },
   },
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/main
->>>>>>> d2f90cedf1ae15b6a4be2def582eb7f514bb1e43
   preview: {
     allowedHosts: [
       'market-mascotas-sfs5.onrender.com'
